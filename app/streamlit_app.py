4ad62c99d4a34c964a2a2f6f1bd0e2e279bf9cb6
RASUL KHAN <praneethkanike2007@gmail.com>
2026-09-09 22:18:02 +0530

    Update WebRTC configuration with redundant Google, Twilio, and OpenRelay STUN servers

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -328,7 +328,17 @@ with col2:
 
 if HAS_WEBRTC:
     RTC_CONFIGURATION = RTCConfiguration(
-        {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+        {
+            "iceServers": [
+                {"urls": ["stun:stun.l.google.com:19302"]},
+                {"urls": ["stun:stun1.l.google.com:19302"]},
+                {"urls": ["stun:stun2.l.google.com:19302"]},
+                {"urls": ["stun:stun3.l.google.com:19302"]},
+                {"urls": ["stun:stun4.l.google.com:19302"]},
+                {"urls": ["stun:global.stun.twilio.com:3478"]},
+                {"urls": ["stun:openrelay.metered.ca:80"]},
+            ]
+        }
     )
 
     class CloudVideoProcessor:
